d62c7b42335e817645c52177686c4fe397bfe7ad
Naresh Tak <taknaresh515@gmail.com>
2023-04-14 15:09:44 +0530

    finaly site was fully complete

--- app.js
+++ app.js
@@ -19,10 +19,9 @@ $(document).ready(function () {
     $('.next-button').click(function () {
         const nextTab = $('.tab-content.current').next('.tab-content');
         const nextIcon = $('.tab-link.active').next('.tab-link');
-
-        if ($('.tab-link').hasClass('active')) {
-            document.getElementById("wrapper").scrollLeft += 25 + $(".tab-link").width()
-        }
+        const tabsContainer = $(".tabs");
+        const currentTab = $(".tab-link.active");
+        const nextTab1 = currentTab.next(".tab-link");
 
         if (nextTab.length == 1) {
             $('.tab-content').removeClass('current');
@@ -32,15 +31,17 @@ $(document).ready(function () {
             nextIcon.addClass('active');
         }
         nextButtonDisable();
+
+        const scrollLeft = (tabsContainer.scrollLeft() + nextTab1.position().left) - 25;
+        tabsContainer.animate({ scrollLeft: scrollLeft }, 100);
     });
 
     $('.prev-button').click(function () {
         const prevTab = $('.tab-content.current').prev('.tab-content');
         const prevIcon = $('.tab-link.active').prev('.tab-link');
-
-        if ($('.tab-link').hasClass('active')) {
-            document.getElementById("wrapper").scrollLeft -= 25 + $(".tab-link").width()
-        }
+        const tabsContainer = $(".tabs");
+        const currentTab = $(".tab-link.active");
+        const prevTab1 = currentTab.prev(".tab-link");
 
         if (prevTab.length == 1) {
             $('.tab-content').removeClass('current');
@@ -50,6 +51,8 @@ $(document).ready(function () {
             prevIcon.addClass('active');
         }
         nextButtonDisable();
+        const scrollLeft = tabsContainer.scrollLeft() + prevTab1.position().left - 200;
+        tabsContainer.animate({ scrollLeft: scrollLeft }, 100);
     });
 });
 
